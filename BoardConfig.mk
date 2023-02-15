8716b3b727603040f626aa8498d87061bc6347a4
ZirgomHaidar <zirgomhaidar17@gmail.com>
2023-02-15 22:59:44 +0530

    Bringup LineageOS

---
+++ BoardConfig.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/oplus/RMX3461
+
+# Include the common OEM chipset BoardConfig.
+include device/oplus/sm8350-common/BoardConfigCommon.mk
+
+# Display
+TARGET_SCREEN_DENSITY := 450
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Recovery
+TARGET_RECOVERY_FSTAB += $(DEVICE_PATH)/init/recovery.fstab
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
+
+# Include the proprietary files BoardConfig.
+include vendor/oplus/RMX3461/BoardConfigVendor.mk
