17a3b54b6decd0a890e1c2eccb6b7ecb32186590
cykeek <cykeek@proton.me>
2023-02-27 05:47:24 +0000

    RMX3461: Update vendorsetup script - Added Hardware/oplus - Added Kernel - Clone hals
    Signed-off-by: cykeek <cykeek@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,37 @@
+#!/bin/bash
+
+banner(){
+clear
+echo "=================================="
+echo "|                                |"
+echo "|   Realme 9 5G SE Setup Script  |"
+echo "|       Branch: Lineage          |"
+echo "|    Maintained By: Cykeek       |"
+echo "|                                |"
+echo "=================================="
+}
+
+banner 2>1
+
 #Clone vibrator HAL
+banner 2>1
 rm -rf vendor/qcom/opensource/vibrator
 git clone https://github.com/itsxrp/android_vendor_qcom_opensource_vibrator.git vendor/qcom/opensource/vibrator
 
-#Clone Bluetooth Module
-#rm -rf packages/modules/Bluetooth
-#git clone https://github.com/itsxrp/android_packages_modules_Bluetooth.git packages/modules/Bluetooth
+# Clone Hardware Oplus
+banner 2>1
+rm -rf hardware/oplus
+git clone https://github.com/Cykeek-Labs/android_hardware_oplus hardware/oplus
+
+# Clone Kernel
+banner 2>1
+git clone https://github.com/Cykeek-Labs/android_kernel_sm8350 kernel/oplus/sm8350
+
+# Clone Media Hals
+banner 2>1
+rm -rf hardware/qcom-caf/sm8350/audio
+rm -rf hardware/qcom-caf/sm8350/media
+rm -rf hardware/qcom-caf/sm8350/display
+git clone https://github.com/Cykeek-Labs/android_hardware_qcom_audio hardware/qcom-caf/sm8350/audio
+git clone https://github.com/Cykeek-Labs/android_hardware_qcom_media hardware/qcom-caf/sm8350/media
+git clone https://github.com/Cykeek-Labs/android_hardware_qcom_display hardware/qcom-caf/sm8350/display
