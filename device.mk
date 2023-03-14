42ce07e9738f65c29674c3942bcd6a6b10911ec1
(mathieuchartier <mathieu.a.chartier@gmail.com>
2023-03-14 14:35:29 +0100

    RMX3461: Speed profile services *  Speed profile services and wifi-service to reduce RAM and storage  Author:    (mathieuchartier <mathieu.a.chartier@gmail.com>

--- device.mk
+++ device.mk
@@ -59,3 +59,6 @@ $(call inherit-product, device/oplus/sm8350-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oplus/RMX3461/RMX3461-vendor.mk)
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
